9aaff303243642ee73bc77970c02dc3ddaa41f79
Allison King <allisonjuliaking@gmail.com>
2023-01-24 12:16:01 -0500

    Experiment: add GA

--- clients/privacy-center/pages/_app.tsx
+++ clients/privacy-center/pages/_app.tsx
@@ -6,6 +6,7 @@ import type { AppProps } from "next/app";
  * Solution found here https://github.com/chakra-ui/chakra-ui/issues/5839#issuecomment-1266493711
  * */
 import { ChakraProvider } from "@chakra-ui/react";
+import Script from "next/script";
 
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/600.css";
@@ -17,9 +18,30 @@ import "../config/config.css";
 import theme from "../theme";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider theme={theme}>
-    <Component {...pageProps} />
-  </ChakraProvider>
+  <>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+    <Script
+      id="google-analytics"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+  page_path: window.location.pathname
+});
+`,
+      }}
+    />
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  </>
 );
 
 export default MyApp;
