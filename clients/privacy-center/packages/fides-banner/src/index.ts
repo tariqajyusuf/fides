645eba591d216ac9d52e83569c4e7208d1f59a5a
Sebastian Sangervasi <sebastian@ethyca.com>
2023-02-28 15:20:56 -0800

    nah microbundle too god

--- clients/privacy-center/packages/fides-banner/src/index.ts
+++ clients/privacy-center/packages/fides-banner/src/index.ts
@@ -5,6 +5,6 @@ import Widget from "./components/Widget";
 const _habitat = habitat(Widget);
 
 _habitat.render({
-  selector: '[data-widget-host="fides-banner"]',
+  selector: "#fides-banner",
   clean: true,
 });
