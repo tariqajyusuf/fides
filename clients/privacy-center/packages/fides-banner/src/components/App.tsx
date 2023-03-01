9be0cb843a0a4190b749b5bc0ff2c56b3dc222a1
Sebastian Sangervasi <sebastian@ethyca.com>
2023-02-28 16:22:00 -0800

    include in demo page

--- clients/privacy-center/packages/fides-banner/src/components/App.tsx
+++
@@ -1,5 +0,0 @@
-export const App = () => (
-  <div id="fides-banner">
-    <h1>We're here</h1>
-  </div>
-);
--- clients/privacy-center/packages/fides-banner/src/components/Widget.tsx
+++ clients/privacy-center/packages/fides-banner/src/components/Widget.tsx
@@ -1,13 +1,9 @@
-import styles from "./styles.css";
-
 type Props = {
   title?: string;
 };
 
-console.log(">>> styles", styles);
-
 export const Widget = ({ title = "Default title" }: Props) => (
-  <div id="fides-banner-widget" className={styles.fidesBannerWidget}>
+  <div id="fides-banner-widget">
     <h1>{title}</h1>
     <h2>Widget rendered</h2>
   </div>
