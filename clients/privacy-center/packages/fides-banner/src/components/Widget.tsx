cc0cc1058d9b0a4bf45f08a8df8a8cb1b510bab6
Sebastian Sangervasi <sebastian@ethyca.com>
2023-02-28 16:07:19 -0800

    css probably wont use

--- clients/privacy-center/packages/fides-banner/src/components/Widget.tsx
+++ clients/privacy-center/packages/fides-banner/src/components/Widget.tsx
@@ -1,11 +1,13 @@
-import "./style.css";
+import styles from "./styles.css";
 
 type Props = {
   title?: string;
 };
 
+console.log(">>> styles", styles);
+
 export const Widget = ({ title = "Default title" }: Props) => (
-  <div id="fides-banner-widget">
+  <div id="fides-banner-widget" className={styles.fidesBannerWidget}>
     <h1>{title}</h1>
     <h2>Widget rendered</h2>
   </div>
